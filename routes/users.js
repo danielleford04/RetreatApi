842d54ef6fd372423a5f3eb2bb34b0d6a778ca8c
danielleford <dford@phizzle.com>
2019-04-17 13:25:55 -0600

    add get users routes

--- routes/users.js
+++ routes/users.js
@@ -93,4 +93,39 @@ router.post("/login", (req, res) => {
     });
 });
 
+/* GET ALL USERS */
+router.get('/', function(req, res, next) {
+    User.find(function (err, users) {
+        if (err) return next(err);
+        res.json(users);
+    });
+});
+
+/* GET SINGLE USER BY ID */
+router.get('/:id', function(req, res, next) {
+    User.findById(req.params.id, function (err, post) {
+        if (err) return next(err);
+        console.log('get',post)
+        post.password = undefined;
+        res.json(post);
+    });
+});
+
+/* UPDATE USER */
+router.put('/:id', function(req, res, next) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+/* DELETE USER */
+router.delete('/:id', function(req, res, next) {
+    User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+
 module.exports = router;
